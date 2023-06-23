b6823a1275ddb942d30d77c4fac07e4fbce99b06
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-23 17:16:17 +0330

    bootstrap file added to every isolated app...

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from "faker";
-
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
-
-document.getElementById("cart-dev").innerHTML = cartText;
+import("./bootstrap");
