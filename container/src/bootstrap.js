a7992d325716b8e1630457c7b46aa916c6a31e20
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-23 18:17:57 +0330

    all apps refactored...

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,11 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount } from "products/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
 
 console.log("Container");
+
+const el = document.querySelector("#my-products");
+
+mount(el);
+
+const cartEl = document.querySelector("#my-cart");
+cartMount(cartEl);
