582d8c842dade1f451443adba844783f5f9c16b2
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-23 18:38:12 +0330

    the apps are connected to each other...

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,11 +1,7 @@
-import { mount } from "products/ProductsIndex";
+import { mount as productsMount } from "products/ProductsIndex";
 import { mount as cartMount } from "cart/CartShow";
 
 console.log("Container");
 
-const el = document.querySelector("#my-products");
-
-mount(el);
-
-const cartEl = document.querySelector("#my-cart");
-cartMount(cartEl);
+productsMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
