e05eb171b30c3c896a058ab3aaf1c6c5031a1861
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-23 17:41:40 +0330

    added some refactor on products app

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,35 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+
+    el.innerHTML = products;
+  }
+};
+
+//? Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFENITLY has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  // Assuming our container doesn't have an element
+  // with id 'dev-production'....
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+//? Context/Situation #2
+// We are running this filr in development or production
+// Through the CONTAINER app
+// No GUARANTEE that an element with an id of 'dev-products'
+// WE DO NOT WANT try to immediately render the app
+export { mount };
