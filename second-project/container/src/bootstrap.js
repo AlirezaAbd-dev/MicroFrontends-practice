204ef26b669964396373ded3ca7a97e9edc1c0ee
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-24 20:55:00 +0330

    react setup is done...

--- second-project/container/src/bootstrap.js
+++ second-project/container/src/bootstrap.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hi There!</h1>);
+root.render(<App />);
